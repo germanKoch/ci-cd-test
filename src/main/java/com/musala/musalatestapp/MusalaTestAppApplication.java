6755ff9a72d592016bbfbae0b0dc43ea048ed568
german <kochgerm@gmail.com>
2022-11-21 00:20:16 +0300

    project structure created

---
+++ src/main/java/com/musala/musalatestapp/MusalaTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.musala.musalatestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusalaTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusalaTestAppApplication.class, args);
+    }
+
+}
