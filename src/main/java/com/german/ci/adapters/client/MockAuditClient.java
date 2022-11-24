f0ce2761434376eef5bea03baff9279a6be93977
german <kochgerm@gmail.com>
2022-11-24 22:05:32 +0300

    feighn client created

--- src/main/java/com/german/ci/adapters/client/MockAuditClient.java
+++ src/main/java/com/german/ci/adapters/client/MockAuditClient.java
@@ -2,17 +2,35 @@ package com.german.ci.adapters.client;
 
 import com.german.ci.domain.general.AuditEvent;
 import com.german.ci.domain.client.AuditClient;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
 @Log4j2
+@RequiredArgsConstructor
 /**
  * It's assumed that audit events should be put to the message broker
  */
 public class MockAuditClient implements AuditClient {
+
+    private final AuditFeign auditFeign;
+
     @Override
     public void sendAuditEvent(AuditEvent auditEvent) {
         log.info("AuditEvent: {}", auditEvent);
+        auditFeign.sendAuditEvent(auditEvent);
+    }
+
+
+    @FeignClient(value = "audit", url = "localhost:8082")
+    interface AuditFeign {
+        @PostMapping(value = "/audit-event", produces = "application/json")
+        void sendAuditEvent(@RequestBody AuditEvent auditEvent);
     }
 }
